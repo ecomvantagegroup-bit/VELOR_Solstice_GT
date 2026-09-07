e427006e5f1f1f5c6de45679c59b0a2122a62042
StellArc24 <ecomvantage.group@gmail.com>
2026-09-07 20:53:49 +0530

    image sequence

--- velor_solstice_GT/src/App.jsx
+++ velor_solstice_GT/src/App.jsx
@@ -1,16 +1,11 @@
+import BackgroundSequence from './components/BackgroundSequence'
 
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'App',
-
-  setup() {
-    return () => (
-      <div class="min-h-screen bg-gray-100 flex items-center justify-center">
-        <h1 class="text-5xl font-bold text-blue-600">
-          Hello Vue JSX + Tailwind!
-        </h1>
+export default {
+  render() {
+    return (
+      <div class="relative min-h-screen">
+        <BackgroundSequence />
       </div>
-    );
-  },
-});
+    )
+  }
+}
